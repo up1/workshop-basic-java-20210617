9300952a991b8f6634822f17f825a1636921e498
up1 <somkiat.p@gmail.com>
2021-06-17 11:42:08 +0700

    Working with command line program

--- demo01/src/Main.java
+++ demo01/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Employee somkiat = new Employee(); // Default constructor
+        String fname = args[0];
+        String lname = args[1];
+        Employee somkiat = new Employee(fname, lname); // Default constructor
 
         String result = somkiat.getFullName();
         System.out.println(result);
