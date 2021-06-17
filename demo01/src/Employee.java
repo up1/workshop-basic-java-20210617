2bb1a8a085c77a35ad8eb2b09f0ca8ccfbdc9dd9
up1 <somkiat.p@gmail.com>
2021-06-17 11:16:45 +0700

    Working with Employee

---
+++ demo01/src/Employee.java
@@ -0,0 +1,21 @@
+public class Employee {
+    // Properties or states
+    private int id;
+    private String fname;
+    private String lname;
+//    Department dep;
+    private String pos;
+
+    // Behaviors or methods
+    void doSmt() {
+
+    }
+
+    public String getFullName() {
+        return fname + " : " + lname;
+    }
+
+    String getPositionName() {
+        return "";
+    }
+}
---
+++ demo01/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Employee somkiat = new Employee(); // Default constructor
+        String result = somkiat.getFullName();
+        System.out.println(result);
+    }
+
+}
