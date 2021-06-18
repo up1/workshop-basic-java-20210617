6a05911c85d3e854f1f43e672d660213fd3217a5
up1 <somkiat.p@gmail.com>
2021-06-18 09:21:03 +0700

    Second case

--- demo01/src/MySortTest.java
+++ demo01/src/MySortTest.java
@@ -18,4 +18,14 @@ class MySortTest {
         assertEquals(expected, results);
     }
 
+    @Test
+    @DisplayName("[1,2] => [1,2]")
+    public void case02() {
+        List<Integer> expected = Arrays.asList(1,2);
+        MySort mySort = new MySort();
+        List<Integer> inputs = Arrays.asList(1,2);
+        List<Integer> results = mySort.sort(inputs);
+        assertEquals(expected, results);
+    }
+
 }
