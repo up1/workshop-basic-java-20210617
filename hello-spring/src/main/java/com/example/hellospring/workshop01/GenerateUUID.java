956c97a668d7bc9c6138a19a5b8e28a4a33fcfb2
up1 <somkiat.p@gmail.com>
2021-06-18 14:31:27 +0700

    Working with overriding

--- hello-spring/src/main/java/com/example/hellospring/workshop01/GenerateUUID.java
+++ hello-spring/src/main/java/com/example/hellospring/workshop01/GenerateUUID.java
@@ -4,9 +4,14 @@ import java.util.Random;
 
 public class GenerateUUID {
 
+    private Random random = new Random();
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
     public String get(String name) {
         String id = "";
-        Random random = new Random();
         int randomNumber = random.nextInt(10);
         id = "XYZ" + name + randomNumber;
         return id;
--- hello-spring/src/test/java/com/example/hellospring/workshop01/GenerateUUIDTest.java
+++ hello-spring/src/test/java/com/example/hellospring/workshop01/GenerateUUIDTest.java
@@ -2,13 +2,23 @@ package com.example.hellospring.workshop01;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+class Random7 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 7;
+    }
+}
 
 class GenerateUUIDTest {
 
     @Test
     public void getUUID() {
         GenerateUUID generateUUID = new GenerateUUID();
+        generateUUID.setRandom(new Random7());
         String uuid = generateUUID.get("somkiat");
         assertEquals("XYZsomkiat7", uuid);
     }
