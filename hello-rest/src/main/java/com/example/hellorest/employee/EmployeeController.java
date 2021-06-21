88ee7cae58a51d4fe1e0de76bf0d1fb36e6f828c
up1 <somkiat.p@gmail.com>
2021-06-21 12:03:42 +0700

    Validate input

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.hellorest.employee;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,12 @@ public class EmployeeController {
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeByID(@PathVariable String id) {
         // Validate id => Number only
-
         int _id = 0;
+        try {
+            _id = Integer.parseInt(id);
+        }catch (NumberFormatException e) {
+            // ERROR => TODO ?
+        }
         return new EmployeeResponse(_id, "Somkiat", "Pui");
     }
 
