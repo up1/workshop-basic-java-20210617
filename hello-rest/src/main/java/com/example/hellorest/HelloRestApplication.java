a01ff1bf18b53b4a5c27962822b37aadfdff1ccf
up1 <somkiat.p@gmail.com>
2021-06-21 15:01:10 +0700

    Initial bean in spring context

--- hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
+++ hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
@@ -1,13 +1,28 @@
 package com.example.hellorest;
 
+import com.example.hellorest.employee.Demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloRestApplication.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(HelloRestApplication.class, args);
+		String[] objects = context.getBeanDefinitionNames();
+		for (String object : objects) {
+			System.out.println(object);
+		}
+		int count = context.getBeanDefinitionCount();
+		System.out.println("Total=" + count);
+
+		Demo demo1 = context.getBean(Demo.class);
+		demo1.setName("From demo 1");
+		Demo demo2 = context.getBean(Demo.class);
+		System.out.println("Demo1 =>" + demo1.getName());
+		System.out.println("Demo2 =>" + demo2.getName());
 	}
 
 }
