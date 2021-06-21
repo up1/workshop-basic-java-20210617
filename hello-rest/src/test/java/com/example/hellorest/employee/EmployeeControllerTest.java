bec2c4d16c84577748af4bb79b502bb2953cda95
up1 <somkiat.p@gmail.com>
2021-06-21 14:10:45 +0700

    First case with spring boot testing

--- hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.hellorest.employee;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
@@ -9,9 +11,25 @@ import static org.springframework.boot.test.context.SpringBootTest.*;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeControllerTest {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     public void callApiWithPathVariable() {
+        EmployeeResponse response
+                = restTemplate.getForObject("/employee/123", EmployeeResponse.class);
+        assertEquals(123, response.getId());
+        assertEquals("Somkiat", response.getFname());
+        assertEquals("Pui", response.getLname());
+    }
 
+    @Test
+    public void callApiWithREquestParameter() {
+        EmployeeResponse response
+                = restTemplate.getForObject("/employee?id=999", EmployeeResponse.class);
+        assertEquals(999, response.getId());
+        assertEquals("Somkiat", response.getFname());
+        assertEquals("Pui", response.getLname());
     }
 
 }
