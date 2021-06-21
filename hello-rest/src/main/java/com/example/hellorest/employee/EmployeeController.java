c4dc1567f0ade1a6570da594a35b504b10b51d44
up1 <somkiat.p@gmail.com>
2021-06-21 13:40:00 +0700

    Working with request parameters

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.hellorest.employee;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Id;
@@ -12,6 +13,19 @@ public class EmployeeController {
 
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeByID(@PathVariable String id) {
+        // Validate id => Number only
+        int _id = 0;
+        try {
+            _id = Integer.parseInt(id);
+        }catch (NumberFormatException e) {
+            // ERROR => TODO ?
+        }
+        return new EmployeeResponse(_id, "Somkiat", "Pui");
+    }
+
+    // employee?id=?
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeByID2(@RequestParam String id) {
         // Validate id => Number only
         int _id = 0;
         try {
