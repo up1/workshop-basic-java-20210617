4955941558e8c53184a9a53fa31c96ca205fdd3a
up1 <somkiat.p@gmail.com>
2021-06-21 09:56:22 +0700

    Start project with gradle

---
+++ hello-rest/src/main/java/com/example/hellorest/HelloRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestApplication.class, args);
+	}
+
+}
---
+++ hello-rest/src/test/java/com/example/hellorest/HelloRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
