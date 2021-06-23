612963edb5764e13b29f1f76d2b84f3dacd38c59
up1 <somkiat.p@gmail.com>
2021-06-23 10:02:26 +0700

    Testing with JPA

---
+++ hello-rest/src/test/java/com/example/hellorest/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.example.hellorest.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @Test
+    public void foundWithID1() {
+        Employee employee1 =  repository.getById(1);
+        assertEquals(1, employee1.getId());
+        assertEquals("Somkiat from DB", employee1.getFirstName());
+        assertEquals("Pui from DB", employee1.getLastName());
+    }
+
+}
