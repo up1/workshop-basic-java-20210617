a2c7e4f46eb3ff1ed508ff2f5bed78e31da2780f
up1 <somkiat.p@gmail.com>
2021-06-23 14:12:44 +0700

    Data model of user api

---
+++ hello-rest/src/main/java/com/example/hellorest/user/Users.java
@@ -0,0 +1,15 @@
+package com.example.hellorest.user;
+
+import java.util.List;
+
+public class Users {
+    private List<UserModel> users;
+
+    public List<UserModel> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserModel> users) {
+        this.users = users;
+    }
+}
